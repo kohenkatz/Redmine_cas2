7c4c7b8c3c484911548e1593aac4724128c9b307
Moshe Katz <moshe@ymkatz.net>
2012-10-11 19:37:05 -0400

    Initial plugin skeleton commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_cas2 do
+  name 'Redmine Cas2 plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
