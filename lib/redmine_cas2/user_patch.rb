7f6429c4bc81dc7320aacf2244edd30f54820fe5
Moshe Katz <moshe@ymkatz.net>
2012-10-14 22:40:39 -0400

    added User patch to prevent CAS users from setting/changing local passwords. (Note that this may require further testing at this point).

---
+++ lib/redmine_cas2/user_patch.rb
@@ -0,0 +1,23 @@
+require_dependency 'project'
+require_dependency 'principal'
+require_dependency 'user'
+
+module Redmine::CAS2
+  module UserPatch
+    def self.included(base) # :nodoc:
+      base.send(:include, InstanceMethods)
+      base.class_eval do
+        unloadable # Mark as unloadable so it is reloaded in development
+        alias_method_chain :change_password_allowed?, :cas
+      end
+    end
+  
+    module InstanceMethods
+      def change_password_allowed_with_cas?
+        (Redmine::CAS2.enabled? && auth_source_id = "CAS") ? false : change_password_allowed_without_cas?
+      end
+    end
+  end
+end
+
+User.send(:include, Redmine::CAS2::UserPatch)
